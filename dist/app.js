886f7aac52176ab3cd9779f97db07692a08b5df2
Leevi Laitala <leevi.laitala@student.lut.fi>
2023-12-07 22:25:13 +0200

    Add week 6

---
+++ dist/app.js
@@ -0,0 +1,55 @@
+"use strict";
+// Followed boilerplate instructions on:
+// https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.use(express_1.default.json());
+let vehicles = [];
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/hello", (req, res) => {
+    res.send("Hello world");
+});
+app.post("/vehicle/add", (req, res) => {
+    let vehicle;
+    if (req.body.bodyType || req.body.wheelCount) {
+        vehicle = req.body;
+    }
+    else if (req.body.draft) {
+        vehicle = req.body;
+    }
+    else if (req.body.wingspan) {
+        vehicle = req.body;
+    }
+    else {
+        vehicle = req.body;
+    }
+    vehicles.push(vehicle);
+    console.log(vehicles);
+    res.status(201).send("Vehicle added");
+});
+app.get("/vehicle/search/:model", (req, res) => {
+    let model = req.params["model"];
+    for (let vehicle of vehicles) {
+        if (vehicle.model === model) {
+            let found = {
+                model: vehicle.model,
+                color: vehicle.color,
+                year: vehicle.year,
+                power: vehicle.power
+            };
+            res.send(found);
+        }
+    }
+    res.status(404);
+});
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,80 @@
+// Followed boilerplate instructions on:
+// https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
+
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+interface Vehicle {
+    model: string;
+    color: string;
+    year: number;
+    power: number;
+}
+
+interface Car extends Vehicle {
+    bodyType: string;
+    wheelCount: number;
+}
+
+interface Boat extends Vehicle {
+    draft: number;
+}
+
+interface Plane extends Vehicle {
+    wingspan: number;
+}
+
+let vehicles: Vehicle[] = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/vehicle/add", (req, res) => {
+    let vehicle;
+
+    if (req.body.bodyType || req.body.wheelCount) {
+        vehicle = req.body as Car;
+    } else if (req.body.draft) {
+        vehicle = req.body as Boat;
+    } else if (req.body.wingspan) {
+        vehicle = req.body as Plane;
+    } else {
+        vehicle = req.body as Vehicle;
+    }
+
+    vehicles.push(vehicle)
+
+    console.log(vehicles);
+    res.status(201).send("Vehicle added");
+});
+
+app.get("/vehicle/search/:model", (req, res) => {
+    let model = req.params["model"];
+
+    for (let vehicle of vehicles) {
+        if (vehicle.model === model) {
+            let found : Vehicle = {
+                model: vehicle.model,
+                color: vehicle.color,
+                year: vehicle.year,
+                power: vehicle.power
+            };
+            res.send(found);
+            return;
+        }
+    }
+
+    res.status(404);
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
